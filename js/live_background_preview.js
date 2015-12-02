bae260ae0f922426e71e775093e82024791cf46d
tomaszantas <tomaszantas@gmail.com>
2015-12-02 23:11:44 +0100

    bug fixed

--- js/live_background_preview.js
+++ js/live_background_preview.js
@@ -2,7 +2,8 @@ $(function () {
 	
 	$('#submit_live_background_preview').on('click', function() {
 		console.log("Background was changed");
-		target = $("#live_background_preview_input").data("target");l() + ')');
+		target = $("#live_background_preview_input").data("target");
+		$(target).css('background-image', 'url(' + $("#live_background_preview_input").val() + ')');
 	});
 
 });
