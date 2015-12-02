af4ab10f950c23ddb0b0caddce7c1280620b0f7b
tomaszantas <tomaszantas@gmail.com>
2015-12-02 22:51:58 +0100

    initial

---
+++ js/live_background_preview.js
@@ -0,0 +1,8 @@
+$(function () {
+	
+	$('#submit_live_background_preview').on('click', function() {
+		console.log("Background was changed");
+		target = $("#live_background_preview_input").data("target");l() + ')');
+	});
+
+});
